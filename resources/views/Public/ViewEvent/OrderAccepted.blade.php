577318e4c1be0d8029ea7c3faeefa357d2f7884c
Khanh <ing.khanh@in-g.jp>
2018-12-03 01:12:08 +0900

    updatre mail

--- resources/views/Public/ViewEvent/OrderAccepted.blade.php
+++ resources/views/Public/ViewEvent/OrderAccepted.blade.php
@@ -9,8 +9,9 @@
         <div class="col-md-4 col-md-offset-4">
             <div class="panel">
                 <div class="panel-body">
-                    <h4 style="text-align: center;">Cám ơn bạn đã xác nhận tham gia Viet Tech Day Tokyo 2018.</h4> <br>
-                        <h5>Chương trình sẽ gửi tới bạn thông tin hướng dẫn chi tiết sau. Bạn nhớ kiểm tra hòm mail nhé!</h5> 
+                    <h4 style="text-align: center;">Cám ơn bạn đã xác nhận tham gia Viet Tech Day Tokyo 2018. <br><br>
+                        Chương trình sẽ gửi tới bạn thông tin hướng dẫn chi tiết sau. Bạn nhớ kiểm tra hòm mail nhé!
+                    </h4>
                 </div>
             </div>
         </div>
